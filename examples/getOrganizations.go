8ca21a200cd44e82037c00ad205533e84edab006
Dexter Park <ddexterpark@icloud.com>
2023-02-07 07:23:33 +0900

    examples: getOrganizations.go
    Retrieve a list of organizations.

---
+++ examples/getOrganizations.go
@@ -0,0 +1,23 @@
+package main
+
+import "github.com/meraki/dashboard-api-go/client"
+import "context"
+import "fmt"
+import "os"
+
+func main() {
+	configuration := client.NewConfiguration()
+	configuration.AddDefaultHeader("X-Cisco-Meraki-API-Key", os.Getenv("MERAKI_DASHBOARD_API_KEY"))
+
+	apiClient := client.NewAPIClient(configuration)
+
+	orgs, _, err := apiClient.ConfigureApi.GetOrganizations(context.Background()).Execute()
+
+	if err != nil {
+		fmt.Println("Meraki API call failed. Details: ", err)
+	}
+
+	for _, org := range orgs {
+		fmt.Printf("%s\n", *org.Name)
+	}
+}
