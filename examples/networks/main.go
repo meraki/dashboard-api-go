b3de18338ab8596c4e33180e096ac0feca662fd3
Francisco Muñoz <fmunoz@cloverhound.com>
2025-01-15 12:36:18 -0600

    Removing key

--- examples/networks/main.go
+++ examples/networks/main.go
@@ -11,9 +11,7 @@ var client *meraki.Client
 func main() {
 	var err error
 	fmt.Println("Authenticating")
-	client, err = meraki.NewClientWithOptions("https://api.meraki.com/",
-		"12f2eb53588c75e28d89e108a05ea0c2487b08cf",
-		"true", "AplicationName VendorName")
+	client, err = meraki.NewClient()
 	if err != nil {
 		fmt.Println(err)
 		return
--- examples/testPagination/main.go
+++ examples/testPagination/main.go
@@ -20,9 +20,7 @@ func simpleCmp(a, b interface{}) bool {
 func main() {
 	var err error
 	fmt.Println("Authenticating")
-	client, err = meraki.NewClientWithOptions("https://api.meraki.com/",
-		"12f2eb53588c75e28d89e108a05ea0c2487b08cf",
-		"true", "AplicationName VendorName")
+	client, err = meraki.NewClient()
 	if err != nil {
 		fmt.Println(err)
 		return
